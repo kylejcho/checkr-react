6b360be8f11770961840d45f828bfa9e9e88ec6e
Kyle Cho <kylejcho@gmail.com>
2022-03-16 18:06:04 -0400

    Create content container and task divs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import ContainerContainer from "./ContentContainer";
-import TasksContainer from "./TasksContainer";
 
 function App() {
   const [tasks, setTasks] = useState([{name: 'Something', description: 'Do this thing', id: 0}])
