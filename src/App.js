fefcf4eece2dfaec13d705fc791c04d90dfd9520
Kyle Cho <kylejcho@gmail.com>
2022-03-21 21:52:37 -0400

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect} from "react";
 import './index.css'
-import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import { checkEmptyGroups, updateSubHeight } from "./components/SubGroup";
 import { updateTaskPosition } from "./components/Task";
+import React, { useState, useEffect} from "react";
 import { addDays } from "date-fns";
 
 export default function App() {
