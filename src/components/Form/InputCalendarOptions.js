fe16c8532ea7a68340bf2ba8793d05c3cfc8dece
Kyle Cho <kylejcho@gmail.com>
2022-05-11 17:06:04 -0400

    Calendar month state

--- src/components/Form/InputCalendarOptions.js
+++ src/components/Form/InputCalendarOptions.js
@@ -1,9 +1,10 @@
-import React, { useRef } from "react"
+import React, { useState } from "react"
 import Calendar from "./Calendar"
+import { getMonth, addMonths, format } from "date-fns"
 import { motion, AnimatePresence } from "framer-motion"
 
 export default function InputCalendarOptions({ openCalendar }) {
-
+    const [month, setMonth] = useState(format(new Date, 'MMM y'))
     return (
         <AnimatePresence>
             {openCalendar && (
@@ -15,7 +16,7 @@ export default function InputCalendarOptions({ openCalendar }) {
                     transition={{duration:0.2}}
                 >
                     <div id="calendarHeader">
-                        <div id="calendarMonth">Mar 2022</div>
+                        <div id="calendarMonth">{month}</div>
                         <div id="calendarArrowContainer">
                             <ion-icon id="backMonthIcon" name="chevron-back-outline" role="img" className="md hydrated" aria-label="chevron back outline"></ion-icon>
                             <ion-icon id="forwardMonthIcon" name="chevron-forward-outline" role="img" className="md hydrated" aria-label="chevron forward outline"></ion-icon>
