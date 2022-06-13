a4f0bb6f04a0bd1bcedd20173d0a51f0031cba22
kylejcho <69482279+kylejcho@users.noreply.github.com>
2022-06-13 01:19:29 -0400

    Loading screen enter exit animations

--- src/components/SidebarContentContainer.js
+++ src/components/SidebarContentContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import Sidebar from './Sidebar'
 import Content from './Content'
+import { motion, AnimatePresence } from 'framer-motion'
 import { auth } from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../firebase'
@@ -72,7 +73,10 @@ export default function SidebarContentContainer({}) {
    //Only get data from firebase once user authentification state has changed
    useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-         if (currentUser) data()
+         if (currentUser)
+            setTimeout(() => {
+               data()
+            }, 1000)
       })
       return unsubscribe()
    }, [])
@@ -80,82 +84,96 @@ export default function SidebarContentContainer({}) {
    //useEffect(() => hideScroll(), [contentType])
 
    //Only render content if user is logged in and tasks state is set
-   if (tasks && auth.currentUser) {
-      return (
-         <div id='sidebarContentContainer'>
-            <Sidebar changeContent={changeContent} contentType={contentType} />
-            <Content
-               contentType={contentType}
-               tasks={tasks}
-               updateTasks={updateTasks}
-               removeTask={removeTask}
-               openTask={openTask}
-               viewTask={viewTask}
-            />
-         </div>
-      )
-   } else
-      return (
-         <div id='loadingScreen'>
-            <div id='loadingScreenLogoContainer'>
-               <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='512'
-                  height='512'
-                  viewBox='0 0 512 512'
+
+   return (
+      <>
+         <AnimatePresence exitBeforeEnter>
+            {!tasks && (
+               <motion.div
+                  id='loadingScreen'
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0, transition: { duration: 0.25 } }}
+                  transition={{ duration: 0.6 }}
                >
-                  <title>ionicons-v5-k</title>
-                  <path
-                     d='M416,221.25V416a48,48,0,0,1-48,48H144a48,48,0,0,1-48-48V96a48,48,0,0,1,48-48h98.75a32,32,0,0,1,22.62,9.37L406.63,198.63A32,32,0,0,1,416,221.25Z'
-                     style={{
-                        fill: 'none',
-                        stroke: '#3880ff',
-                        strokeLinejoin: 'round',
-                        strokeWidth: '50px',
-                     }}
-                  />
-                  <path
-                     d='M256,56V176a32,32,0,0,0,32,32H408'
-                     style={{
-                        fill: 'none',
-                        stroke: '#3880ff',
-                        strokeLinecap: 'round',
-                        strokeLinejoin: 'round',
-                        strokeWidth: '50px',
-                     }}
-                  />
-                  <line
-                     x1='176'
-                     y1='288'
-                     x2='336'
-                     y2='288'
-                     style={{
-                        fill: 'none',
-                        stroke: '#3880ff',
-                        strokeLinecap: 'round',
-                        strokeLinejoin: 'round',
-                        strokeWidth: '50px',
-                     }}
-                  />
-                  <line
-                     x1='176'
-                     y1='368'
-                     x2='336'
-                     y2='368'
-                     style={{
-                        fill: 'none',
-                        stroke: '#3880ff',
-                        strokeLinecap: 'round',
-                        strokeLinejoin: 'round',
-                        strokeWidth: '50px',
-                     }}
-                  />
-               </svg>
-               <p>checkr.</p>
+                  <div id='loadingScreenLogoContainer'>
+                     <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='512'
+                        height='512'
+                        viewBox='0 0 512 512'
+                     >
+                        <title>ionicons-v5-k</title>
+                        <path
+                           d='M416,221.25V416a48,48,0,0,1-48,48H144a48,48,0,0,1-48-48V96a48,48,0,0,1,48-48h98.75a32,32,0,0,1,22.62,9.37L406.63,198.63A32,32,0,0,1,416,221.25Z'
+                           style={{
+                              fill: 'none',
+                              stroke: '#3880ff',
+                              strokeLinejoin: 'round',
+                              strokeWidth: '50px',
+                           }}
+                        />
+                        <path
+                           d='M256,56V176a32,32,0,0,0,32,32H408'
+                           style={{
+                              fill: 'none',
+                              stroke: '#3880ff',
+                              strokeLinecap: 'round',
+                              strokeLinejoin: 'round',
+                              strokeWidth: '50px',
+                           }}
+                        />
+                        <line
+                           x1='176'
+                           y1='288'
+                           x2='336'
+                           y2='288'
+                           style={{
+                              fill: 'none',
+                              stroke: '#3880ff',
+                              strokeLinecap: 'round',
+                              strokeLinejoin: 'round',
+                              strokeWidth: '50px',
+                           }}
+                        />
+                        <line
+                           x1='176'
+                           y1='368'
+                           x2='336'
+                           y2='368'
+                           style={{
+                              fill: 'none',
+                              stroke: '#3880ff',
+                              strokeLinecap: 'round',
+                              strokeLinejoin: 'round',
+                              strokeWidth: '50px',
+                           }}
+                        />
+                     </svg>
+                     <p>checkr.</p>
+                  </div>
+                  <div id='loadingCircle'></div>
+               </motion.div>
+            )}
+         </AnimatePresence>
+         {tasks && (
+            <div id='sidebarContentContainer'>
+               <Sidebar
+                  changeContent={changeContent}
+                  contentType={contentType}
+               />
+               <Content
+                  contentType={contentType}
+                  tasks={tasks}
+                  updateTasks={updateTasks}
+                  removeTask={removeTask}
+                  openTask={openTask}
+                  viewTask={viewTask}
+               />
             </div>
-            <div id='loadingCircle'></div>
-         </div>
-      )
+         )}
+      </>
+   )
 }
 
 function hideScroll() {
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -54,94 +54,92 @@ function Task({ task, subTasks, updateSubTasks, viewTask }) {
    const boxShadow = RaisedShadow(y)
 
    return (
-      <>
-         <AnimatePresence>
-            {showTask && (
-               <Reorder.Item
-                  id={task.id}
-                  ref={taskContainer}
-                  className={`taskContainer`}
-                  value={task}
-                  style={{ boxShadow, y }}
-                  whileDrag={{ scale: 1.04 }}
-                  transition={{ duration: 0.25 }}
-                  onDragStart={() => {
-                     taskContainer.current.classList.add('dragging')
-                  }}
-                  onDragEnd={() => {
-                     updateSubTasks(subTasks)
-                     taskContainer.current.classList.remove('dragging')
-                  }}
-                  exit={{ opacity: 0, transition: { duration: 0.3 } }}
-                  dragTransition={{ bounceStiffness: 1000, bounceDamping: 70 }}
+      <AnimatePresence>
+         {showTask && (
+            <Reorder.Item
+               id={task.id}
+               ref={taskContainer}
+               className={`taskContainer`}
+               value={task}
+               style={{ boxShadow, y }}
+               whileDrag={{ scale: 1.04 }}
+               transition={{ duration: 0.25 }}
+               onDragStart={() => {
+                  taskContainer.current.classList.add('dragging')
+               }}
+               onDragEnd={() => {
+                  updateSubTasks(subTasks)
+                  taskContainer.current.classList.remove('dragging')
+               }}
+               exit={{ opacity: 0, transition: { duration: 0.3 } }}
+               dragTransition={{ bounceStiffness: 1000, bounceDamping: 70 }}
+               onClick={(e) => {
+                  e.stopPropagation()
+                  updateSubTasks(subTasks)
+                  viewTask(task)
+                  setSelectTask(!selectTask)
+                  document
+                     .querySelectorAll('.taskContainer')
+                     .forEach((taskContainer) =>
+                        taskContainer.classList.remove('viewing')
+                     )
+                  taskContainer.current.classList.add('viewing')
+               }}
+            >
+               <CheckCircle
+                  task={task}
+                  taskContainer={taskContainer}
+                  checkTask={checkTask}
+                  complete={complete}
+                  checkClickAnimation={checkClickAnimation}
+               />
+               <div className='nameContainer'>{task.name}</div>
+               <div
+                  className='deleteContainer'
                   onClick={(e) => {
+                     handleDeleteClick()
                      e.stopPropagation()
-                     updateSubTasks(subTasks)
-                     viewTask(task)
-                     setSelectTask(!selectTask)
-                     document
-                        .querySelectorAll('.taskContainer')
-                        .forEach((taskContainer) =>
-                           taskContainer.classList.remove('viewing')
-                        )
-                     taskContainer.current.classList.add('viewing')
                   }}
                >
-                  <CheckCircle
-                     task={task}
-                     taskContainer={taskContainer}
-                     checkTask={checkTask}
-                     complete={complete}
-                     checkClickAnimation={checkClickAnimation}
-                  />
-                  <div className='nameContainer'>{task.name}</div>
-                  <div
-                     className='deleteContainer'
-                     onClick={(e) => {
-                        handleDeleteClick()
-                        e.stopPropagation()
-                     }}
+                  <svg
+                     xmlns='http://www.w3.org/2000/svg'
+                     width='20'
+                     height='20'
+                     viewBox='0 0 512 512'
                   >
-                     <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='20'
-                        height='20'
-                        viewBox='0 0 512 512'
-                     >
-                        <title>ionicons-v5-l</title>
-                        <line
-                           x1='368'
-                           y1='368'
-                           x2='144'
-                           y2='144'
-                           style={{
-                              fill: 'none',
-                              stroke: '#697384',
-                              strokeLinecap: 'round',
-                              strokeLinejoin: 'round',
-                              strokeWidth: '32px',
-                           }}
-                        />
-                        <line
-                           x1='368'
-                           y1='144'
-                           x2='144'
-                           y2='368'
-                           style={{
-                              fill: 'none',
-                              stroke: '#697384',
-                              strokeLinecap: 'round',
-                              strokeLinejoin: 'round',
-                              strokeWidth: '32px',
-                           }}
-                        />
-                     </svg>
-                  </div>
-                  <div className='descriptionContainer'>{task.description}</div>
-               </Reorder.Item>
-            )}
-         </AnimatePresence>
-      </>
+                     <title>ionicons-v5-l</title>
+                     <line
+                        x1='368'
+                        y1='368'
+                        x2='144'
+                        y2='144'
+                        style={{
+                           fill: 'none',
+                           stroke: '#697384',
+                           strokeLinecap: 'round',
+                           strokeLinejoin: 'round',
+                           strokeWidth: '32px',
+                        }}
+                     />
+                     <line
+                        x1='368'
+                        y1='144'
+                        x2='144'
+                        y2='368'
+                        style={{
+                           fill: 'none',
+                           stroke: '#697384',
+                           strokeLinecap: 'round',
+                           strokeLinejoin: 'round',
+                           strokeWidth: '32px',
+                        }}
+                     />
+                  </svg>
+               </div>
+               <div className='descriptionContainer'>{task.description}</div>
+            </Reorder.Item>
+         )}
+      </AnimatePresence>
    )
 }
 
